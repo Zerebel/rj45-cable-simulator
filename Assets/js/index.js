4ed0f0920071193070ea777bf52d4592b66566fd
Zerebel <kofitruth11@gmail.com>
2022-10-31 09:36:17 +0000

    cursor follower created

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -2,48 +2,41 @@ const colorSelector = document.querySelectorAll('.color-pick');
 const colorChanger = document.querySelectorAll('.color-change');
 const selected = document.querySelector('#selected-color');
 
-let currentX;
-let currentY;
-let initialX;
-let initialY;
-let xOffset = 0;
-let yOffset = 0;
-
 const dragItem = (e) => {
   selected.style.left = e.pageX + 'px';
   selected.style.top = e.pageY - 60 + 'px';
 };
-
 document.addEventListener('mousemove', dragItem, false);
+
 let color = 'bg-white';
 const getClickedItem = () =>
   colorSelector.forEach((button) => {
     button.addEventListener('click', (e) => {
       if (button.classList.contains('bg-orange-white'))
-        return (
-          (color = 'bg-orange-white-repeat') &&
-          selected.classList.add('bg-orange-white-repeat')
-        );
+        return setSelectedColor('bg-orange-white-repeat');
       if (button.classList.contains('bg-[#ffa500]'))
-        return (
-          (color = 'bg-[#ffa500]') && selected.classList.add('bg-[#ffa500]')
-        );
+        return setSelectedColor('bg-[#ffa500]');
       if (button.classList.contains('bg-green-white'))
-        return (color = 'bg-green-white-repeat');
+        return setSelectedColor('bg-green-white-repeat');
       if (button.classList.contains('bg-[#008000]'))
-        return (color = 'bg-[#008000]');
+        return setSelectedColor('bg-[#008000]');
       if (button.classList.contains('bg-[#0000ff]'))
-        return (color = 'bg-[#0000ff]');
+        return setSelectedColor('bg-[#0000ff]');
       if (button.classList.contains('bg-blue-white'))
-        return (color = 'bg-blue-white-repeat');
+        return setSelectedColor('bg-blue-white-repeat');
       if (button.classList.contains('bg-[#9d5f04]'))
-        return (color = 'bg-[#9d5f04]');
+        return setSelectedColor('bg-[#9d5f04]');
       if (button.classList.contains('bg-brown-white'))
-        return (color = 'bg-brown-white-repeat');
+        return setSelectedColor('bg-brown-white-repeat');
     });
   });
 getClickedItem();
 
+const setSelectedColor = (newColor) => {
+  selected.classList.remove(...selected.classList);
+  selected.classList.add(newColor, 'w-6', 'h-6', 'rounded-full', 'absolute');
+  return (color = newColor);
+};
 const changeItemColor = () =>
   colorChanger.forEach((wire) => {
     wire.addEventListener('click', (e) => {
